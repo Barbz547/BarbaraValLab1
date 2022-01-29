118768d025b0597a8be266f886bc8a24e2b9ebc0
barbz <barbz@Barb.attlocal.net>
2022-01-29 00:24:50 -0500

    JUnit Testing

--- src/main/java/first/Barb/lab1/RoboVac.java
+++ src/main/java/first/Barb/lab1/RoboVac.java
@@ -3,11 +3,13 @@
  */
 package first.Barb.lab1;
 
+import edu.fiu.jit.EmptyComponent;
+
 /**
  * @author barbz
  *
  */
-public class RoboVac {
+public class RoboVac extends EmptyComponent  {
 	
 	private String brand;  
 	private double price; // price varies from as low as $50 to as high as $1200
@@ -19,7 +21,15 @@ public class RoboVac {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		RoboVac mynewinstance = new RoboVac();
+		System.out.println(mynewinstance);
+	}
 
+
+	@Override
+	public String getComponentName() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
